348fff876645dd1d3bc9f720c62a4900e60dcad4
Philip N <phi.nguyen@laverne.edu>
2018-02-09 17:33:53 -0800

    Add event listener for magic btn

---
+++ popup.js
@@ -0,0 +1,6 @@
+var magicBtn = document.querySelector('.magic-btn');
+var body = document.querySelector('.body');
+magicBtn.addEventListener('click', function() {
+  console.log('clicke!!');
+  // body.style.color = 'red';
+});
