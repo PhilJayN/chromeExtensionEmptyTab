efd5434dea80d50a7759dadba8d45591f69919ed
Philip N <phi.nguyen@laverne.edu>
2018-02-09 20:14:27 -0800

    Add event listener only on DOMContentLoaded

---
+++ empty.js
@@ -0,0 +1,9 @@
+//For empty.html
+
+console.log('empty js running!');
+var emptyBody = document.querySelector('.empty-body');
+console.log(emptyBody);
+magicBtn.addEventListener('click', function() {
+  console.log('clicke!!');
+  emptyBody.style.backgroundColor = 'red';
+});
--- popup.js
+++ popup.js
@@ -1,17 +1,31 @@
-var magicBtn = document.querySelector('.magic-btn');
+console.log('popup js running rrrrrrrrr');
+
 
 //For popup.html
-var popupBody = document.querySelector('.popup-body');
-console.log(popupBody);
+// var popupBody = document.querySelector('.popup-body');
+// console.log(popupBody);
 // magicBtn.addEventListener('click', function() {
 //   console.log('clicke!!');
 //   popupBody.style.backgroundColor = 'red';
 // });
 
-//For empty.html
-var emptyBody = document.querySelector('.empty-body');
-console.log(emptyBody);
-magicBtn.addEventListener('click', function() {
-  console.log('clicke!!');
-  emptyBody.style.backgroundColor = 'red';
+
+document.addEventListener('DOMContentLoaded', function () {
+  var popupBody = document.querySelector('.popup-body');
+  var magicBtn = document.querySelector('.magic-btn');
+  console.log('popup el', popupBody);
+  console.log('clicke on btn!');
+  popupBody.style.backgroundColor = 'green';
 });
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   var divs = document.querySelectorAll('div');
+//   for (var i = 0; i < divs.length; i++) {
+//     divs[i].addEventListener('click', click);
+//   }
+// });
