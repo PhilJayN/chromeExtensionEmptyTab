3b4d65650f4d23b69cf9354268e01a9818c1ada8
Philip N <phi.nguyen@laverne.edu>
2018-02-09 18:31:20 -0800

    Change class names for various elements

--- popup.js
+++ popup.js
@@ -1,6 +1,17 @@
 var magicBtn = document.querySelector('.magic-btn');
-var body = document.querySelector('.main-body');
+
+//For popup.html
+var popupBody = document.querySelector('.popup-body');
+console.log(popupBody);
+// magicBtn.addEventListener('click', function() {
+//   console.log('clicke!!');
+//   popupBody.style.backgroundColor = 'red';
+// });
+
+//For empty.html
+var emptyBody = document.querySelector('.empty-body');
+console.log(emptyBody);
 magicBtn.addEventListener('click', function() {
   console.log('clicke!!');
-  body.style.backgroundColor = 'red';
+  emptyBody.style.backgroundColor = 'red';
 });
